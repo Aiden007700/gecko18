dab97a6b82acaa23504ebcba3bc54d6af4c3b54e
Aiden <campbell.e.aiden@gmail.com>
2018-11-18 20:54:24 +0100

    bp

---
+++ .next/static/webpack/static/development/pages/app/new.js.6e61e007ae192baf8662.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static/development/pages/app/new.js",{
+
+/***/ "./pages/app/new/index.jsx":
+/*!*********************************!*\
+  !*** ./pages/app/new/index.jsx ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/aiden/webdev/gecko18/pages/app/new/index.jsx";
+
+
+var New = function New() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "New");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (New);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/app/new")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=new.js.6e61e007ae192baf8662.hot-update.js.map
